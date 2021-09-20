f2d6eec9c2b6e75401382c0fba49366ff75f2de8
DanielGomez <danielgomezaristizabal@gmail.com>
2021-09-20 13:04:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
     let y=event.key2;
     x=parseFloat(x);
     y=parseFloat(y);
-    let suma= x-y;
+    let suma= x*y;
     const response = {
         statusCode: 200,
         body: suma,
